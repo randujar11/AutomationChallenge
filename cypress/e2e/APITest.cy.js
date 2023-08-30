0b54c993375613bd7ce556169f84591603c3f2a7
Randy Andujar <randujar.11@gmail.com>
2023-08-30 19:12:53 -0400

    Delete APITest

--- cypress/e2e/APITest.cy.js
+++ cypress/e2e/APITest.cy.js
@@ -2,7 +2,7 @@ describe('API Automation', () =>{
     
     it('GET method',()=>{
 
-        cy.postAPI('https://gorest.co.in/public/v2/users',)
+        //cy.postAPI('https://gorest.co.in/public/v2/users',)
 
     })
 })
